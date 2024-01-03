c191eab3e2708ca54fa4f9292167f99a8efb4ca7
heresue <sue.synpark@gmail.com>
2024-01-03 18:21:40 +0900

    1/3

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,15 +1,29 @@
-new Swiper(".swiper-container", {
-    direction: "vertical", // 수직 슬라이드
-    autoplay: true, // 자동 재생 여부
-    loop: true, // 반복 재생 여부
-  });
+// fp-section
+var swiper = new Swiper(".swiperFp", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 
 // best-section
-var swiper = new Swiper(".swiper-best", {
-  slidesPerView: 4,
-  spaceBetween: 30,
+var swiper = new Swiper(".swiperBest", {
+  slidesPerView: 2,
   centeredSlides: true,
+  spaceBetween: 20,
+  freeMode: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -18,13 +32,3 @@ var swiper = new Swiper(".swiper-best", {
 
 
 
-var swiper = new Swiper(".swiper-best", {
-  direction: "vertical",
-  slidesPerView: 1,
-  spaceBetween: 30,
-  mousewheel: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
