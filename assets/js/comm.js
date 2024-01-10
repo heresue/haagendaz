79b04e84fb634fd83b90adc1400866b3d502e160
heresue <sue.synpark@gmail.com>
2024-01-10 18:17:15 +0900

    1/10

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,3 +1,51 @@
+// header
+        // 준비이벤트 (위에 넣어줄 때는 준비이벤트를 넣어줘야 읽힘)
+        $(function () {
+          // 헤더 색 바꿔주기
+          // $('.header-fix').css('background', 'white');
+
+          // 전역변수 (전체)
+          const $window = $(window);
+          const $header = $('#header');
+
+          $header.each(function () {
+              // header의 정수리(top) 위치값(offset)
+              // let headerOffsetTop = $header.offset().top;
+              // 모든 페이지의 헤더값이 아닌 지금 나의 엄마를 잡고 싶다면?
+              let headerOffsetTop = $(this).offset().top;
+              
+              // console.log(headerOffsetTop)
+              // console.log("header의 위치값 " + headerOffsetTop)
+
+              // scroll event
+              // window가 스크롤 됐을 때 ([참고] .on: jQuery에서 "addEventListener"의 기능)
+              // scroll 이벤트가 발생할 때 한 번만 호출되도록 수정 (off스크롤)
+              $window.off("scroll").on("scroll", function () {
+                  // 윈도우의 스크롤 확인
+                  const $scrolled = $window.scrollTop()
+                  // console.log("윈도우의 스크롤 값 : " + $scrolled)
+
+                  // 윈도우의 스크롤 값이 header의 위치값보다 커졌을 때
+                  if ($scrolled > headerOffsetTop) {
+                      // 참일 경우
+                      // header에 .sticky 추가
+                      $header.addClass('sticky')
+                      // $header.addClass('sticky') 대신 gsap 넣어도 된다
+                  }else {
+                      // 거짓일 경우
+                      // header에 .sticky 제거
+                      $header.removeClass('sticky')
+                  }
+              })
+              
+              // trigger: 스크롤 이벤트를 강제로 발생시키는 역할
+              //      이미 발생한 것 같이 보여줌 (새로고침했을때 sticky가 계속 적용되어 있음)
+              $window.trigger("scroll")
+          })
+      })
+
+
+
 // intro-section
 var swiper = new Swiper(".swiperIntro", {
   spaceBetween: 0,
@@ -40,6 +88,70 @@ var swiper = new Swiper(".swiperBest", {
 
 
 
+
+// ytb-section
+
+
+// const videoWrap = document.getElementById('video-wrap');
+
+// // Adjust video size on scroll
+// window.addEventListener('scroll', () => {
+//     const scrollPercentage = window.scrollY / (document.body.scrollHeight - window.innerHeight);
+//     const newWidth = 560 + scrollPercentage * 400; // Adjust the width as needed
+//     const newHeight = 315 + scrollPercentage * 225; // Adjust the height as needed
+
+//     videoWrap.style.width = `${newWidth}px`;
+//     videoWrap.style.height = `${newHeight}px`;
+// });
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const ytbSection = document.querySelector(".ytb-section");
+//   const videoWrap = document.querySelector(".ytb_wrap");
+
+//   window.addEventListener("scroll", function () {
+//     const scrollPercentage = window.scrollY / (ytbSection.offsetHeight - window.innerHeight);
+//     const scale = 1 + scrollPercentage * 0.5; // 조절 가능한 확대 비율
+
+
+//     // 동영상 크기 조절
+//     videoWrap.style.transform = `scale(${scale})`;
+
+//     // 동영상의 크기에 따라 섹션의 높이 재조정 (섹션의 높이에 따라 스크롤 높이 계산)
+//     ytbSection.style.height = `${100 * scale}vh`;
+//   });
+// });
+
+// const ytbSection = document.querySelector(".ytb-section");
+// const ytbWrap = document.querySelector(".ytb_wrap");
+// const scrollPercentage = window.scrollY / (ytbSection.offsetHeight - window.innerHeight);
+// const clipPathValue = 0 + scrollPercentage * 50; // 예시: 10에서 60 사이의 값으로 변경
+// ytbWrap.style.clipPath = `inset(${clipPathValue}%)`;
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const ytbSection = document.querySelector(".ytb-section");
+  const ytbWrap = document.querySelector(".ytb_wrap");
+
+  window.addEventListener("scroll", function () {
+    const ytbSectionTop = ytbSection.offsetTop;
+    const ytbWrapTop = ytbWrap.offsetTop;
+    let bounds = ytbSection.getBoundingClientRect();
+    const scrollPercentage = window.scrollY / (ytbSection.offsetHeight - window.innerHeight);
+
+    if (bounds.top < window.innerHeight && bounds.bottom >= 0) {
+      // 스크롤 값이 ytb-section의 시작 위치를 넘어갈 때
+      const clipPathValue = 0 + scrollPercentage * 50; // 예시: 10에서 60 사이의 값으로 변경
+      ytbWrap.style.clipPath = `inset(${clipPathValue}%)`;
+    }
+  });
+});
+
+
+
+
+
 // fp-section
 // 윈도우에 스크롤 이벤트를 걸어줌
 window.addEventListener("scroll", function () {
@@ -63,6 +175,9 @@ window.addEventListener("scroll", function () {
       }
       })
 })
+
+
+
 
 
 
